0032808951f249bb90e2da5df3641c942f89e6ca
Ciro García <ciruelo@gmail.com>
2022-01-10 16:58:53 +0100

    Initial commit

---
+++ src/main/java/me/kolterdyx/chemicalwarfare/ChemicalWarfare.java
@@ -0,0 +1,17 @@
+package me.kolterdyx.chemicalwarfare;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChemicalWarfare extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
